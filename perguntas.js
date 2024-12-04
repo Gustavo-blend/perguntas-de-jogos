2ee3c7c81d9cefeb23d8124b2e257e857a6ca4fa
Gustavo-blend <00001090842077sp@al.educacao.sp.gov.br>
2024-12-04 08:26:47 -0300

    adicionando arquivos do meu projeto de perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'jogo de videogame',
+    'Qual é o vilão principal de god of war 3',
+    'é o zeus'
+)
+
+criaCartao(
+    'jogo de videogame',
+    'no GTA 5 online, qual é o golpe que da mais dinheiro ?',
+    'é o golpe do cayo perico'
+)
+
+criaCartao(
+    'jogo de videogame',
+    'em pokemon TCG qual é a carta mais rara',
+    'é a do mew que você precisa pegar todas as cartas de kanto'
+)
+
+criaCartao(
+    'jogo de videogame',
+    'Qual foi o jogo do ano de 2022',
+    'é o elden ring'
+)
